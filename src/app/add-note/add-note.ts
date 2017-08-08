885497f0d6054e9b9b9c4feef7e3578c7b61ec34
TheOpti <wieckowski.piotr.93@gmail.com>
2017-08-09 00:51:23 +0200

    Add new note component

--- src/app/add-note/add-note.ts
+++ src/app/add-note/add-note.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  HostListener
+} from '@angular/core';
 
 @Component({
   selector: 'add-note',
   templateUrl: 'add-note.html',
   styleUrls: ['add-note.scss']
 })
-export class AddNoteComponent {}
+export class AddNoteComponent {
+
+  constructor(private eRef: ElementRef) { }
+
+  @ViewChild('textbox') textbox: any;
+  @ViewChild('add-note-component') component: any;
+
+  private isAddingNewNote = false;
+  post = '';
+  title = '';
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      this.enableEditing();
+    } else {
+      this.clearInputs();
+      this.disableEditing();
+    }
+  }
+
+  enableEditing() {
+    this.isAddingNewNote = true;
+  }
+
+  disableEditing() {
+    this.isAddingNewNote = false;
+  }
+
+  clearInputs() {
+    this.title = '';
+    this.post = '';
+  }
+
+  adjustTextarea() {
+    this.textbox.nativeElement.style.height = '1px';
+    this.textbox.nativeElement.style.height =
+      (25 + this.textbox.nativeElement.scrollHeight) + "px";
+  }
+
+  addNewNote() {
+    this.clearInputs();
+    this.disableEditing();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import {
   MdSidenavModule,
@@ -48,7 +48,8 @@ const appRoutes: Routes = [
     MdSidenavModule,
     MdToolbarModule,
     MdIconModule,
-    MdButtonModule
+    MdButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
