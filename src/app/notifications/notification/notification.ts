2d5cbf690701b8689ee78ecde07b8ae00b32c10c
TheOpti <wieckowski.piotr.93@gmail.com>
2017-08-11 17:20:53 +0200

    Notifications - progress bar, hiding notifications after 6 s

--- src/app/notifications/notification/notification.ts
+++ src/app/notifications/notification/notification.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+const notificationsTypesMap = {
+  'note-delete': 'This note has been deleted',
+  'note-add': 'You added a new note'
+};
 
 @Component({
   selector: 'notification',
@@ -6,4 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['notification.scss']
 })
 export class NotificationComponent {
+
+  private notificationTypeMessage: string;
+  @Input() type: string;
+  @Input() id: string;
+  @Output() delete: EventEmitter<any> = new EventEmitter();
+
+  ngOnInit() {
+    this.notificationTypeMessage = notificationsTypesMap[this.type];
+
+    setTimeout(() => {
+      this.delete.emit(this.id);
+    }, 6000);
+  }
+
+  closeNotification() {
+    this.delete.emit(this.id);
+  }
 }
--- src/app/notifications/notifications.ts
+++ src/app/notifications/notifications.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
 
+import v1 from 'uuid/v1';
+
 @Component({
   selector: 'notifications',
   templateUrl: 'notifications.html',
   styleUrls: ['notifications.scss']
 })
 export class NotificationsComponent {
+
+  private notifications: any;
+
+  ngOnInit() {
+    this.notifications = [
+      {
+        id: v1(),
+        type: 'note-delete'
+      }
+    ];
+  }
+
+  deleteNotification(notificationId) {
+    this.notifications = this.notifications
+      .filter(notification => notification.id !== notificationId);
+  }
+
 }
