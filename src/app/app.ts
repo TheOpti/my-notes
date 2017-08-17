98b75a7831f458fcce5ec7efdfe94fd38205b331
TheOpti <wieckowski.piotr.93@gmail.com>
2017-08-17 11:04:02 +0200

    Show/hide add note component depending on route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, Event} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['app.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  private canShowAddNoteComponent: boolean;
+
+  constructor(private router: Router) {
+    this.canShowAddNoteComponent = true;
+  }
+
+  ngOnInit() {
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .subscribe((event:Event) => {
+        if (event instanceof NavigationEnd) {
+          this.checkCanShowAddNote(event.url);
+        }
+      });
+  }
+
+  checkCanShowAddNote(url) {
+    if (url.includes('tag') || url === '/notes' || url === '/reminders' || url === '/') {
+      this.canShowAddNoteComponent = true;
+    } else {
+      this.canShowAddNoteComponent = false;
+    }
+  }
+
 }
