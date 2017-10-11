339fbbdbd4b1a68b58a280f3746e81366a93dc99
TheOpti <wieckowski.piotr.93@gmail.com>
2017-10-11 12:19:39 +0200

    Password forgot form with validation

--- src/app/views/login/forms/password-forgot-form/password-forgot-form.ts
+++ src/app/views/login/forms/password-forgot-form/password-forgot-form.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,9 +9,42 @@ import { Router } from '@angular/router';
 })
 export class PasswordForgotFormComponent {
 
-  constructor(private router: Router) { }
+  public form: FormGroup;
+  public submitted: boolean;
+  public showError: boolean;
+  public events: any[] = [];
 
-  resetPassword() {
+  constructor(private router: Router, private _fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.form = this._fb.group({
+      login: ['', [<any>Validators.required]],
+      email: ['', [<any>Validators.required]],
+      password: ['', [<any>Validators.required]],
+      passwordRepeat: ['', [<any>Validators.required]]
+    });
+
+    this.subcribeToFormChanges();
+  }
+
+
+  subcribeToFormChanges() {
+    const formStatusChanges$ = this.form.statusChanges;
+    const formValueChanges$ = this.form.valueChanges;
+
+    formStatusChanges$.subscribe(x => this.events.push({ event: 'STATUS_CHANGED', object: x }));
+    formValueChanges$.subscribe(x => this.events.push({ event: 'VALUE_CHANGED', object: x }));
+  }
+
+
+  resetPassword(model) {
+    this.submitted = true;
+
+    if (model.email === 'foo') {
+      this.showError = true;
+      return;
+    }
+
     console.log('resetPassword()');
   }
 
