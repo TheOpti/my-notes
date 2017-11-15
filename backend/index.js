7275767a1b95854d8b749a866d87968276e5c03d
TheOpti <wieckowski.piotr.93@gmail.com>
2017-11-15 23:39:23 +0100

    [Backend] Add connection to DB - Postgres

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,31 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import Sequelize from 'sequelize';
 
 import TestController from './controllers/TestController';
 import LoginController from './controllers/LoginController';
 import NotesController from './controllers/NotesController';
 
+// set up
 const app = express();
-
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
+
+// database
+const sequelize = new Sequelize('postgres://postgres@localhost:5432/my-notes');
+sequelize
+  .authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
+
+// controllers
 app.use(TestController);
 app.use(LoginController);
 app.use(NotesController);
+
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
