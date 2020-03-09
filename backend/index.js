a0b601255b8c7f4b56ec0e08099f8173089aac48
TheOpti <wieckowski.piotr.93@gmail.com>
2020-03-09 23:18:19 +0100

    Add connection to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,28 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import sequelize from './sequelize';
 
-import LoginController from './Controllers/LoginController';
-import UserController from './Controllers/UserController';
-import NotesController from './Controllers/NotesController';
-import TagsController from './Controllers/TagsController';
+import dotenv from 'dotenv';
 
-import initAllRelations from './init-relations';
-import initData from './init-data';
+import { establishConnectionToDatabase } from './mongo';
 
 // set up
 const app = express();
+dotenv.config();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 // allow CORS
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Token');
   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
   next();
 });
 
-// establish connection to DB
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-
-    // create relations and tables
-    initAllRelations();
-    sequelize.sync({force: true}).then(() => {
-      initData();
-    });
-  })
-  .catch(err => console.error('Unable to connect to the database:', err));
-
-// use Controllers
-app.use(LoginController);
-app.use(UserController);
-app.use(NotesController);
-app.use(TagsController);
-
 // start the app
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
+
+  establishConnectionToDatabase();
 });
