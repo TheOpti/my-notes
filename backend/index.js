9c95fd2acc5ee1c955cd11892baa7936bbc3bddb
TheOpti <wieckowski.piotr.93@gmail.com>
2020-03-15 23:31:59 +0100

    Add Jest and supertest to backend

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,6 @@
-import express from 'express';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
+import app from './app';
 
-import dotenv from 'dotenv';
-
-import { establishConnectionToDatabase } from './mongo';
-import { seedUserData } from './utils/data';
-import router from './routes';
-
-// set up
-const app = express();
-dotenv.config();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-
-// allow CORS
-app.use((_, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Token');
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  next();
-});
-
-// define routes
-app.use('/', router);
-
-// start the app
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
-
   establishConnectionToDatabase();
-
-  // populate DB with data
-  seedUserData();
 });
