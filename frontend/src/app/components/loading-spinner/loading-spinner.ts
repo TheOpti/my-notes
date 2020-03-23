8b8c7936803f420327e767ec401d05d156747067
TheOpti <wieckowski.piotr.93@gmail.com>
2020-03-23 18:54:39 +0100

    Remove old folder with Angular

--- frontend/src/app/components/loading-spinner/loading-spinner.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'loading-spinner',
-  templateUrl: 'loading-spinner.html',
-  styleUrls: ['loading-spinner.scss']
-})
-export class LoadingSpinnerComponent {}
